a2e986ddef5ab54b5cf73ce5ab7201c8093a5ef8
divyapatel1409 <159720838+divyapatel1409@users.noreply.github.com>
2025-02-28 23:25:47 -0500

    Implment Toggle Sidebar

--- src/components/RequestResponsePanelWrapper.jsx
+++ src/components/RequestResponsePanelWrapper.jsx
@@ -4,26 +4,28 @@ import RequestPanel from "./RequestPanel";
 import ResponsePanel from "./ResponsePanel";
 
 /* Left Bottom block component HTML */
-const RequestResponsePanelWrapper = () => {
+const RequestResponsePanelWrapper = ({ isSidebarVisible }) => {
+  const { topHeight, handleMouseDown } = useResizablePanel();
+  const cssClass = isSidebarVisible
+    ? "flex flex-col flex-grow"
+    : "flex flex-col flex-grow pl-10";
+    
+  return (
+    <div className={cssClass}>
+      {/* Right panel top block - Request Block */}
+      <RequestPanel topHeight={topHeight} />
 
-    const { topHeight, handleMouseDown } = useResizablePanel();
+      {/* Resizable Divider */}
+      <div
+        className="border-gray-300"
+        style={{ height: "1px", cursor: "ns-resize", background: "#d1d5dc" }}
+        onMouseDown={handleMouseDown}
+      ></div>
 
-    return (
-        <div className="flex flex-col flex-grow">
-
-            {/* Right panel top block - Request Block */}
-            <RequestPanel topHeight={topHeight} />
-
-            {/* Resizable Divider */}
-            <div className="border-gray-300"
-            style={{ height: "1px", cursor: "ns-resize", background: "#d1d5dc"}}
-            onMouseDown={handleMouseDown}
-            ></div>
-
-            {/* Right panel bottom block - Response Block */}
-            <ResponsePanel />
-        </div>
-    );
+      {/* Right panel bottom block - Response Block */}
+      <ResponsePanel />
+    </div>
+  );
 };
 
 export default RequestResponsePanelWrapper;
